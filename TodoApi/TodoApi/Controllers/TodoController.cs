43bce2cc905cf22bb2960a835390007f2a6f7ad3
Chris Munoz <cmunoz@extraspace.com>
2020-10-28 21:11:29 -0600

    Clean up

--- TodoApi/TodoApi/Controllers/TodoController.cs
+++ TodoApi/TodoApi/Controllers/TodoController.cs
@@ -1,89 +1,48 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
-
-//// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-//namespace TodoApi.Controllers
-//{
-//    [Route("api/[controller]")]
-//    public class TodoController : Controller
-//    {
-//        // GET: api/<controller>
-//        [HttpGet]
-//        public IEnumerable<string> Get()
-//        {
-//            return new string[] { "value1", "value2" };
-//        }
-
-//        // GET api/<controller>/5
-//        [HttpGet("{id}")]
-//        public string Get(int id)
-//        {
-//            return "value";
-//        }
-
-//        // POST api/<controller>
-//        [HttpPost]
-//        public void Post([FromBody]string value)
-//        {
-//        }
-
-//        // PUT api/<controller>/5
-//        [HttpPut("{id}")]
-//        public void Put(int id, [FromBody]string value)
-//        {
-//        }
-
-//        // DELETE api/<controller>/5
-//        [HttpDelete("{id}")]
-//        public void Delete(int id)
-//        {
-//        }
-//    }
-//}
-
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using TodoApi.Models;
 using System.Linq;
+using TodoApi.Models;
 
 namespace TodoApi.Controllers
-
 {
     [Route("api /[controller]")]
     [ApiController]
     public class TodoController : Controller
     {
         private readonly TodoContext _context;
+
         public TodoController(TodoContext context)
         {
             _context = context;
+
             if (_context.TodoItems.Count() == 0)
             {
                 _context.TodoItems.Add(new TodoItem { Name = "Item1" });
                 _context.SaveChanges();
             }
         }
+
         // GET /api/todo
         [HttpGet]
         public IEnumerable<TodoItem> GetAll()
         {
             return _context.TodoItems.ToList();
         }
+
         // GET /api/todo/{id}
         [HttpGet("{id}", Name = "GetTodo")]
         public IActionResult GetById(long id)
         {
             var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+
             if (item == null)
             {
                 return NotFound();
             }
+
             return new ObjectResult(item);
         }
+
         [HttpPost]
         public IActionResult Create([FromBody] TodoItem item)
         {
@@ -91,8 +50,10 @@ namespace TodoApi.Controllers
             {
                 return BadRequest();
             }
+
             _context.TodoItems.Add(item);
             _context.SaveChanges();
+
             return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
 
         }
@@ -104,15 +65,19 @@ namespace TodoApi.Controllers
             {
                 return BadRequest();
             }
+
             var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+
             if (todo == null)
             {
                 return NotFound();
             }
+
             todo.IsComplete = item.IsComplete;
             todo.Name = item.Name;
             _context.TodoItems.Update(todo);
             _context.SaveChanges();
+
             return new NoContentResult();
         }
     }
