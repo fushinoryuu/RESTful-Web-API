e9bed262748a8b74372d2735c4c55c6c118a48eb
Chris Munoz <cmunoz@extraspace.com>
2020-10-28 21:24:09 -0600

    Working on getting the project to use the controller, service, repo

---
+++ TodoApi/TodoApi/Services/ValuesService.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoApi.Services
+{
+    public class ValuesService
+    {
+        public ValuesService()
+        {
+        }
+
+        public string GetValueById(int id)
+        {
+            return "value";
+        }
+    }
+}
